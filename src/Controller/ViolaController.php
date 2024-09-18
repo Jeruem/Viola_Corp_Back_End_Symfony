1478869db97ee479786116f9bcab2dfc6ec2f495
Jeruem <louisjeremy81@gmail.com>
2024-09-18 10:49:05 +0200

    Add serialiser Viola Controller

--- src/Controller/ViolaController.php
+++ src/Controller/ViolaController.php
@@ -7,8 +7,11 @@ use App\Repository\ViolaRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 
 #[Route('/api/viola', name: 'app_api_viola_')]
@@ -16,48 +19,57 @@ use Symfony\Component\Routing\Attribute\Route;
 class ViolaController extends AbstractController
 {
 
-    public function __construct(private EntityManagerInterface $manager, private ViolaRepository $repository)
+    public function __construct(private EntityManagerInterface $manager, private ViolaRepository $repository, private SerializerInterface $serializer, private UrlGeneratorInterface $urlGenerator)
     {
     }
     #[Route(methods: 'POST')]
-    public function new(): Response
+    public function new(Request $request): JsonResponse
     {
-        $viola = new Viola();
-        $viola->setName('Viola Corp');
-        $viola->setDescription('Ce savoir-faire inégalé dans la fabrication de nos instruments');
+        $viola = $this->serializer->deserialize($request->getContent(), Viola::class, 'json');
         $viola->setCreatedAt(new DateTimeImmutable());
-        // Tell Doctrine you want to (eventually) save the restaurant (no queries yet)
         $this->manager->persist($viola);
-        // Actually executes the queries (i.e. the INSERT query)
         $this->manager->flush();
-        return $this->json(
-            ['message' => "Viola resource created with {$viola->getId()} id"],
-            Response::HTTP_CREATED,
+        $responseData = $this->serializer->serialize($viola, 'json');
+        $location = $this->urlGenerator->generate(
+            'app_api_viola_show',
+            ['id' => $viola->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL,
         );
+        return new JsonResponse($responseData, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
     #[Route('/{id}', name: 'show', methods: 'GET')]
-    public function show(int $id): Response
+    public function show(int $id): JsonResponse
     {
         $viola = $this->repository->findOneBy(['id' => $id]);
-        if (!$viola) {
-            throw $this->createNotFoundException("No shop found for {$id} id");
+        if ($viola) {
+            $responseData = $this->serializer->serialize($viola, 'json');
+
+            return new JsonResponse($responseData, Response::HTTP_OK, [], true);
         }
-        return $this->json(
-            ['message' => "A Shop was found : {$viola->getName()} for {$viola->getId()} id"]
-        );
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
     #[Route('/{id}', name: 'edit', methods: 'PUT')]
-    public function edit(int $id): Response
+    public function edit(int $id, Request $request): JsonResponse
     {
         $viola = $this->repository->findOneBy(['id' => $id]);
-        if (!$viola) {
-            throw $this->createNotFoundException("No Shop found for {$id} id");
+        if ($viola) {
+            $viola = $this->serializer->deserialize(
+                $request->getContent(),
+                Viola::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $viola]
+            );
+            $viola->setUpdatedAt(new DateTimeImmutable());
+
+            $this->manager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
         }
-        $viola->setName('Shop name updated');
-        $this->manager->flush();
-        return $this->redirectToRoute('app_api_viola_show', ['id' => $viola->getId()]);
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 
     #[Route('/{id}', name: 'delete', methods: 'DELETE')]
